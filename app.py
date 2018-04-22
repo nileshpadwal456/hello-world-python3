7c6c12c9950082f7485c8c2ee93be9ce8af79ff7
Rami Kiriako <rmkiriako@gmail.com>
2018-04-22 11:30:14 -0700

    Host the app on either a specified port or 5000
    - Heroku will set the $PORT variable per:
      https://devcenter.heroku.com/articles/dynos#web-dynos

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
@@ -8,4 +10,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
